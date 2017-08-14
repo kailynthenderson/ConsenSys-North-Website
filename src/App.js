a00291af0047a75c9ae9599e3e1d6a0e1d54b947
Kailyn Henderson <kailyn_henderson@sympatico.ca>
2017-08-14 13:38:51 -0400

    Finished ConsenSys North Site

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import SignUpForm from './components/signUpForm.js'
 import Particles from 'react-particles-js';
 import Snackbar from 'material-ui/Snackbar';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 
-import ConsensysLogo from './images/consensysLogo.svg'
-import TorontoSilhouette from './images/torontoSilhouette.png'
-import LinkedInIcon from './images/linkedinIcon.svg'
+import ConsensysLogo from './images/consensysLogo.svg';
+import TorontoSilhouette from './images/torontoSilhouette.png';
+import LinkedInIcon from './images/linkedinIcon.svg';
+
+import {amber50} from 'material-ui/styles/colors';
 
 import './App.css';
 
@@ -23,6 +24,7 @@ const styles = {
     position: "absolute",
     fontFamily: '"Lucida Sans Unicode", "Lucida Grande", sans-serif',
     opacity: 0.6,
+    color: amber50,
   },
   textStyle: {
     position: 'relative',
@@ -75,11 +77,15 @@ const styles = {
    fontSize: '1.8vh',
  },
  inputStyle: {
-   color: "black",
+   color: amber50,
    fontFamily: '"Lucida Sans Unicode", "Lucida Grande", sans-serif',
  },
  iconStyle: {
-   verticalAlign: "middle"
+   verticalAlign: "middle",
+   opacity: 0.5,
+ },
+ contactNameStyle: {
+   verticalAlign: "middle",
  },
 }
 
@@ -120,20 +126,32 @@ class App extends Component {
         <div style={styles.textStyle}>
           <h1 style={styles.headerStyle}>CONSENSYS TORONTO</h1>
           <p>Receive news and upcoming blockchain events right here in Toronto</p>
-          <TextField
-            className="emailInput"
-            floatingLabelText="Email"
-            hintText="canada@consensys.net"
-            floatingLabelStyle={styles.inputStyle}
-            underlineFocusStyle={styles.inputStyle}
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-          <br/>
-          <RaisedButton label="Sign Up" primary={true}
-            className="signUpButton"
-            onTouchTap={this.handleTouchTap}
-          />
+          <form action="//surge.us16.list-manage.com/subscribe/post?u=40488fe6b08303175dc1b61bc&amp;id=58fcc5cf87" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
+            <TextField
+              type="email"
+              name="EMAIL"
+              className="required email"
+              className="emailInput"
+              id="mce-EMAIL"
+              floatingLabelText="Email"
+              hintText="canada@consensys.net"
+              floatingLabelStyle={styles.inputStyle}
+              underlineFocusStyle={styles.inputStyle}
+              value={this.state.value}
+              onChange={this.handleChange}
+              autoComplete="off"
+            />
+            <br/>
+            <RaisedButton label="Sign Up" primary={true}
+              className="signUpButton"
+              onTouchTap={this.handleTouchTap}
+              type="submit"
+              value="Subscribe"
+              name="subscribe"
+              id="mc-embedded-subscribe"
+            />
+            <div style={{position: 'absolute', left: '-5000px', ariaHidden: true}}><input type="text" name="b_40488fe6b08303175dc1b61bc_58fcc5cf87" tabIndex="-1" value=""/></div>
+          </form>
         </div>
         <Snackbar
           style={{bottom: 0, textAlign: 'center',}}
@@ -142,26 +160,26 @@ class App extends Component {
           autoHideDuration={4000}
           onRequestClose={this.handleRequestClose}
         />
-        {/*
-          <p style={styles.contactTextStyle}>
-            <a href="mailto:chami.akmeemana@consensys.net">
-            Chami Akmeemana
-            </a>
-            |
-            <a href="mailto:russell.verbeeten@consensys.net">
-            Russell Verbeeten
-            </a>
-          </p>
-        */}
         <p style={styles.contactTextStyle}>
-          Chami
-          <a href="https://www.linkedin.com/in/chami1" target="_blank">
-            <img style={styles.iconStyle} src={LinkedInIcon} alt="ConsenSys Logo" weight="20px" height="20px"/>
+          <a href="mailto:chami.akmeemana@consensys.net" style={{color: amber50, textDecoration: 'none'}}>
+            <span style={styles.contactNameStyle}>Chami</span>
           </a>
-          |
-          Russell
-          <a href="https://ca.linkedin.com/in/russellverbeeten" target="_blank">
-            <img style={styles.iconStyle} src={LinkedInIcon} alt="ConsenSys Logo" weight="20px" height="20px"/>
+          <a href="https://www.linkedin.com/in/chami1" target="_blank" rel="noopener noreferrer">
+            <img style={styles.iconStyle} src={LinkedInIcon} alt="LinkedIn Icon" width="20px" height="20px"/>
+          </a>
+          {' | '}
+          <a href="mailto:russell.verbeeten@consensys.net" style={{color: amber50, textDecoration: 'none'}}>
+            <span style={styles.contactNameStyle}>Russell</span>
+          </a>
+          <a href="https://ca.linkedin.com/in/russellverbeeten" target="_blank" rel="noopener noreferrer">
+            <img style={styles.iconStyle} src={LinkedInIcon} alt="LinkedIn Icon" width="20px" height="20px"/>
+          </a>
+          {' | '}
+          <a href="mailto:henry.chan@consensys.net" style={{color: amber50, textDecoration: 'none'}}>
+            <span style={styles.contactNameStyle}>Henry</span>
+          </a>
+          <a href="https://www.linkedin.com/in/henry-chan-85b6b227" target="_blank" rel="noopener noreferrer">
+            <img style={styles.iconStyle} src={LinkedInIcon} alt="LinkedIn Icon" width="20px" height="20px"/>
           </a>
         </p>
         <p style={styles.consensysTextStyle}>A ConsenSys Formation © 2017</p>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {grey900} from 'material-ui/styles/colors';
+import {amber50, cyan200, grey900} from 'material-ui/styles/colors';
 import { MuiThemeProvider } from 'material-ui/styles';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import injectTapEventPlugin from 'react-tap-event-plugin';
@@ -12,7 +12,7 @@ injectTapEventPlugin();
 const muiTheme = getMuiTheme({
   palette: {
     textColor: grey900,
-    primary1Color: grey900,
+    primary1Color: cyan200,
   },
 });
 
